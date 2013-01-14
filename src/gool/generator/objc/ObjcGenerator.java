1ac766a0553412f5062e66e4423c3fbd8d2654c7
montantj <montantj@pcnt-217-12>
2013-01-14 12:45:03 +0100

    ajout des squelettes de classes pour l'objective C

---
+++ src/gool/generator/objc/ObjcGenerator.java
@@ -0,0 +1,266 @@
+package gool.generator.objc;
+
+import gool.ast.constructs.ClassNew;
+import gool.ast.constructs.CustomDependency;
+import gool.ast.constructs.Dependency;
+import gool.ast.constructs.EnhancedForLoop;
+import gool.ast.constructs.EqualsCall;
+import gool.ast.constructs.MainMeth;
+import gool.ast.constructs.ParentCall;
+import gool.ast.constructs.ToStringCall;
+import gool.ast.list.ListAddCall;
+import gool.ast.list.ListContainsCall;
+import gool.ast.list.ListGetCall;
+import gool.ast.list.ListGetIteratorCall;
+import gool.ast.list.ListIsEmptyCall;
+import gool.ast.list.ListRemoveAtCall;
+import gool.ast.list.ListRemoveCall;
+import gool.ast.list.ListSizeCall;
+import gool.ast.map.MapContainsKeyCall;
+import gool.ast.map.MapEntryGetKeyCall;
+import gool.ast.map.MapEntryGetValueCall;
+import gool.ast.map.MapGetCall;
+import gool.ast.map.MapGetIteratorCall;
+import gool.ast.map.MapIsEmptyCall;
+import gool.ast.map.MapPutCall;
+import gool.ast.map.MapRemoveCall;
+import gool.ast.map.MapSizeCall;
+import gool.ast.system.SystemCommandDependency;
+import gool.ast.system.SystemOutDependency;
+import gool.ast.system.SystemOutPrintCall;
+import gool.ast.type.TypeBool;
+import gool.ast.type.TypeChar;
+import gool.ast.type.TypeDecimal;
+import gool.ast.type.TypeEntry;
+import gool.ast.type.TypeInt;
+import gool.ast.type.TypeList;
+import gool.ast.type.TypeMap;
+import gool.ast.type.TypeObject;
+import gool.ast.type.TypeString;
+import gool.generator.common.CommonCodeGenerator;
+
+public class ObjcGenerator extends CommonCodeGenerator{
+
+	@Override
+	public void addCustomDependency(String key, Dependency value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getCode(ClassNew classNew) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(EnhancedForLoop enhancedForLoop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(EqualsCall equalsCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListAddCall lac) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListContainsCall lcc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListGetCall lgc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListGetIteratorCall lgic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListIsEmptyCall liec) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListRemoveAtCall lrc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListRemoveCall lrc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ListSizeCall lsc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MainMeth mainMeth) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapContainsKeyCall mapContainsKeyCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapEntryGetKeyCall mapEntryGetKeyCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapEntryGetValueCall mapEntryGetKeyCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapGetCall mapGetCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapGetIteratorCall mapGetIteratorCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapIsEmptyCall mapIsEmptyCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapPutCall mapPutCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapRemoveCall mapRemoveCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(MapSizeCall mapSizeCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ParentCall parentCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(SystemOutDependency systemOutDependency) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(SystemOutPrintCall systemOutPrintCall) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(ToStringCall tsc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeBool typeBool) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeDecimal typeReal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeEntry typeEntry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeInt typeInt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeList typeList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeMap typeMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeObject typeObject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeString typeString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(CustomDependency customDependency) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(SystemCommandDependency systemCommandDependency) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCode(TypeChar typeChar) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
