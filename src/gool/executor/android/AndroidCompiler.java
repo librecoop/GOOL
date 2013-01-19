2768f3c3a15c92a74bb24541e5256b98c25fa2e7
vandenberg <andrevandenberg101@gmail.com>
2013-01-19 12:09:24 +0100

    Added code to read from LogCat the output String for JUnit testing, TODO: App not running automatically on adk, String manipulation can possibly be done at a better place

--- src/gool/executor/android/AndroidCompiler.java
+++ src/gool/executor/android/AndroidCompiler.java
@@ -34,101 +34,135 @@ public class AndroidCompiler extends SpecificCompiler {
 		List<String> params = new ArrayList<String>();
 		params.add("ant");
 		params.add("debug");
-//		addSdkToClasspath();
-//		if (mainFile == null) {
-//			mainFile = files.get(0);
-//		}
-//		for (File file : files) {
-//			params.add(file.toString());
-//		}
-//		if (args != null) {
-//			params.addAll(args);
-//		}
-		Command.exec(new File(Settings.get("android_out_dir_final")), params);	
+		// addSdkToClasspath();
+		// if (mainFile == null) {
+		// mainFile = files.get(0);
+		// }
+		// for (File file : files) {
+		// params.add(file.toString());
+		// }
+		// if (args != null) {
+		// params.addAll(args);
+		// }
+		Command.exec(new File(Settings.get("android_out_dir_final")), params);
 		return (new File(Settings.get("android_out_dir_final")));
 	}
-	
+
 	@Override
-	public File compileToObjectFile(List<File> files,  File mainFile,
-			List<File> classPath, List<String> args) throws FileNotFoundException {
+	public File compileToObjectFile(List<File> files, File mainFile,
+			List<File> classPath, List<String> args)
+			throws FileNotFoundException {
 		return compileToExecutable(files, mainFile, classPath, args);
 	}
-	
 
 	@Override
 	public String run(File file, List<File> classPath)
 			throws FileNotFoundException {
-		try{
-			
+		try {
+
 			List<String> paramsToRunEmulator = new ArrayList<String>();
 			List<String> paramsToExecuteApk = new ArrayList<String>();
-			ProcessBuilder pb=new ProcessBuilder(paramsToRunEmulator);
+			ProcessBuilder pb = new ProcessBuilder(paramsToRunEmulator);
 			paramsToRunEmulator.add("android");
 			paramsToRunEmulator.add("avd");
 			pb.directory(new File(Settings.get("android_sdk_path")));
 			Process p = pb.redirectErrorStream(true).start();
 			int retval = p.waitFor();
 			if (retval != 0) {
-					throw new CommandException("The command execution returned "
-							+ retval + " as return value... !\n");
-							}
+				throw new CommandException("The command execution returned "
+						+ retval + " as return value... !\n");
+			}
 			paramsToExecuteApk.add("adb");
 			paramsToExecuteApk.add("install");
-			paramsToExecuteApk.add(Settings.get("android_out_dir_final")+"//bin//AndroidProject-debug.apk");
-			return Command.exec(new File(Settings.get("android_sdk_path")), paramsToExecuteApk);
-		}
-		catch (IOException e) {
+			paramsToExecuteApk.add((new File(Settings
+					.get("android_out_dir_final"))).getAbsolutePath()
+					+ "//bin//AndroidProject-debug.apk");
+
+			String commandResult = Command.exec(new File(Settings.get("android_sdk_path")),
+					paramsToExecuteApk);
+			System.out.println(commandResult);
+
+			 String returnString = null;
+			 try {
+			 String executeCommand = "adb logcat -d raw JUnitSysOut:I *:S";
+			 Process waitForProcess =
+			 Runtime.getRuntime().exec(executeCommand);
+			
+			 waitForProcess.waitFor();
+			 BufferedReader buf = new BufferedReader(new InputStreamReader(
+			 waitForProcess.getInputStream()));
+			 String line = "";
+			 String output = "";
+			
+			 while ((line = buf.readLine()) != null) {
+			 output += line + "\n";
+			 }
+			 returnString = output;
+			
+			 } catch (IOException e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
+			 } catch (InterruptedException e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
+			 }
+			
+			 return returnString.substring(returnString.indexOf(":")+1);
+		} catch (IOException e) {
 			throw new CommandException(e);
 		} catch (InterruptedException e) {
 			throw new CommandException("It seems the process was killed", e);
 		}
-		//addSdkToClasspath();
-		
+		// addSdkToClasspath();
+
 	}
 
-	private void addSdkToClasspath(){
+	private void addSdkToClasspath() {
 		/*
 		 * Add the classpath
 		 */
 		try {
-			
-			String addSdkDir="export PATH=$PATH:"+Settings.get("android_sdk_location")+"/tools:"+Settings.get("android_sdk_location")+"/platform-tools";
-			addSdkDir.concat(Settings.get("android_sdk_location")+"/platform-tools");
+
+			String addSdkDir = "export PATH=$PATH:"
+					+ Settings.get("android_sdk_location") + "/tools:"
+					+ Settings.get("android_sdk_location") + "/platform-tools";
+			addSdkDir.concat(Settings.get("android_sdk_location")
+					+ "/platform-tools");
 			FileWriter fstream = new FileWriter("android.sh");
-	        BufferedWriter out = new BufferedWriter(fstream);
-	        
-		    out.write(addSdkDir);
-		    out.newLine();
-		    out.write("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
-		    out.close();
-		    Runtime.getRuntime().exec("sh android.sh");
-		    Runtime.getRuntime().exec("ant debug");
-//		    Runtime.getRuntime().exec("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
-//			ProcessBuilder pb = new ProcessBuilder("android.sh");		
-//			Map<String,String> env=pb.environment();
-//			Process p = pb.redirectErrorStream(true).start();
-//			p.getOutputStream().close();
-//			BufferedReader in = new BufferedReader(new InputStreamReader(p
-//					.getInputStream()));
-//			StringBuffer buffer = new StringBuffer();
-//			String line;
-//			while ((line = in.readLine()) != null) {
-//				buffer.append(line).append("\n");
-//			}
-//	
-//			int retval = p.waitFor();
-//	
-//			if (retval != 0) {
-//				throw new CommandException("The command execution returned "
-//						+ retval + " as return value... !\n" + buffer);
-//			}
+			BufferedWriter out = new BufferedWriter(fstream);
+
+			out.write(addSdkDir);
+			out.newLine();
+			out.write("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
+			out.close();
+			Runtime.getRuntime().exec("sh android.sh");
+			Runtime.getRuntime().exec("ant debug");
+			// Runtime.getRuntime().exec("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
+			// ProcessBuilder pb = new ProcessBuilder("android.sh");
+			// Map<String,String> env=pb.environment();
+			// Process p = pb.redirectErrorStream(true).start();
+			// p.getOutputStream().close();
+			// BufferedReader in = new BufferedReader(new InputStreamReader(p
+			// .getInputStream()));
+			// StringBuffer buffer = new StringBuffer();
+			// String line;
+			// while ((line = in.readLine()) != null) {
+			// buffer.append(line).append("\n");
+			// }
+			//
+			// int retval = p.waitFor();
+			//
+			// if (retval != 0) {
+			// throw new CommandException("The command execution returned "
+			// + retval + " as return value... !\n" + buffer);
+			// }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		catch (InterruptedException e) {
-//			throw new CommandException("It seems the process was killed", e);
-//		}
+		// catch (InterruptedException e) {
+		// throw new CommandException("It seems the process was killed", e);
+		// }
 	}
 
 	@Override
@@ -137,6 +171,4 @@ public class AndroidCompiler extends SpecificCompiler {
 		return null;
 	}
 
-	
-	
 }
