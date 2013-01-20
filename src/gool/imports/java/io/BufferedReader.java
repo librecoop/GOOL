414e52b47c2b02de83487d65334bd85f2ae9de74
ankuj <ankujarora@gmail.com>
2013-01-20 15:24:01 -0500

    changes to constructor, also implemented method readLine

--- src/gool/imports/java/io/BufferedReader.java
+++ src/gool/imports/java/io/BufferedReader.java
@@ -1,7 +1,11 @@
 package gool.imports.java.io;
-
+import gool.imports.java.io.FileReader;
 public class BufferedReader {
-	public BufferedReader (FileReader arg) {
+	private BufferedReader (FileReader arg) {
 		// TODO Auto-generated method stub
+	}
+	public String readLine()
+	{
+		return null;
 	}
 }
