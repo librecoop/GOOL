3684384641794552124ceaa77dd07c297aabd860
vandenberg <andrevandenberg101@gmail.com>
2013-01-21 10:15:30 +0100

    Fixed code that got deleted somehow, added LogCat output reading back in

--- src/gool/executor/android/AndroidCompiler.java
+++ src/gool/executor/android/AndroidCompiler.java
@@ -5,16 +5,18 @@ import gool.ast.system.SystemOutPrintCall;
 import gool.executor.Command;
 import gool.executor.CommandException;
 import gool.executor.common.SpecificCompiler;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AndroidCompiler extends SpecificCompiler {
 
-	public AndroidCompiler(File androidOutputDir, List<File> deps)
-	{
+	public AndroidCompiler(File androidOutputDir, List<File> deps) {
 		super(androidOutputDir, deps);
 	}
 
@@ -25,144 +27,177 @@ public class AndroidCompiler extends SpecificCompiler {
 		List<String> params = new ArrayList<String>();
 		params.add("ant");
 		params.add("debug");
-//		addSdkToClasspath();
-//		if (mainFile == null) {
-//			mainFile = files.get(0);
-//		}
-//		for (File file : files) {
-//			params.add(file.toString());
-//		}
-//		if (args != null) {
-//			params.addAll(args);
-//		}
-		Command.exec(new File(Settings.get("android_out_dir_final")), params);	
+		// addSdkToClasspath();
+		// if (mainFile == null) {
+		// mainFile = files.get(0);
+		// }
+		// for (File file : files) {
+		// params.add(file.toString());
+		// }
+		// if (args != null) {
+		// params.addAll(args);
+		// }
+		Command.exec(new File(Settings.get("android_out_dir_final")), params);
 		return (new File(Settings.get("android_out_dir_final")));
 	}
-	
+
 	@Override
-	public File compileToObjectFile(List<File> files,  File mainFile,
-			List<File> classPath, List<String> args) throws FileNotFoundException {
+	public File compileToObjectFile(List<File> files, File mainFile,
+			List<File> classPath, List<String> args)
+			throws FileNotFoundException {
 		return compileToExecutable(files, mainFile, classPath, args);
 	}
-	
 
 	@Override
 	public String run(File file, List<File> classPath)
 			throws FileNotFoundException {
-		try{
-			
+		try {
+
 			List<String> paramsToRunEmulator = new ArrayList<String>();
-			ProcessBuilder pbForEmulator=new ProcessBuilder(paramsToRunEmulator);
+			ProcessBuilder pbForEmulator = new ProcessBuilder(
+					paramsToRunEmulator);
 			paramsToRunEmulator.add("android");
 			paramsToRunEmulator.add("avd");
 			pbForEmulator.directory(new File(Settings.get("android_sdk_path")));
-			Process avdManagerStart = pbForEmulator.redirectErrorStream(true).start();
+			Process avdManagerStart = pbForEmulator.redirectErrorStream(true)
+					.start();
 			int retval = avdManagerStart.waitFor();
-				if (retval != 0)
-				{
-						throw new CommandException("The command execution returned "
-								+ retval + " as return value... !\n");
-				}
+			if (retval != 0) {
+				throw new CommandException("The command execution returned "
+						+ retval + " as return value... !\n");
+			}
 			installApkOnPhone();
 			runApkOnPhone();
-			return "Still need to do";
-		}
-		catch (IOException e) {
+			return readLogCatFile(); // Reads the logCat file and returns the
+										// SysOut equivalent.
+		} catch (IOException e) {
 			throw new CommandException(e);
 		} catch (InterruptedException e) {
 			throw new CommandException("It seems the process was killed", e);
 		}
-		//addSdkToClasspath();
-		
+		// addSdkToClasspath();
+
 	}
 
-//	private void addSdkToClasspath(){
-//		/*
-//		 * Add the classpath
-//		 */
-//		try {
-//			
-//			String addSdkDir="export PATH=$PATH:"+Settings.get("android_sdk_location")+"/tools:"+Settings.get("android_sdk_location")+"/platform-tools";
-//			addSdkDir.concat(Settings.get("android_sdk_location")+"/platform-tools");
-//			FileWriter fstream = new FileWriter("android.sh");
-//	        BufferedWriter out = new BufferedWriter(fstream);
-//	        
-//		    out.write(addSdkDir);
-//		    out.newLine();
-//		    out.write("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
-//		    out.close();
-//		    Runtime.getRuntime().exec("sh android.sh");
-//		    Runtime.getRuntime().exec("ant debug");
-//		    Runtime.getRuntime().exec("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
-//			ProcessBuilder pb = new ProcessBuilder("android.sh");		
-//			Map<String,String> env=pb.environment();
-//			Process p = pb.redirectErrorStream(true).start();
-//			p.getOutputStream().close();
-//			BufferedReader in = new BufferedReader(new InputStreamReader(p
-//					.getInputStream()));
-//			StringBuffer buffer = new StringBuffer();
-//			String line;
-//			while ((line = in.readLine()) != null) {
-//				buffer.append(line).append("\n");
-//			}
-//	
-//			int retval = p.waitFor();
-//	
-//			if (retval != 0) {
-//				throw new CommandException("The command execution returned "
-//						+ retval + " as return value... !\n" + buffer);
-//			}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		catch (InterruptedException e) {
-//			throw new CommandException("It seems the process was killed", e);
-//		}
-//	}
+	// private void addSdkToClasspath(){
+	// /*
+	// * Add the classpath
+	// */
+	// try {
+	//
+	// String
+	// addSdkDir="export PATH=$PATH:"+Settings.get("android_sdk_location")+"/tools:"+Settings.get("android_sdk_location")+"/platform-tools";
+	// addSdkDir.concat(Settings.get("android_sdk_location")+"/platform-tools");
+	// FileWriter fstream = new FileWriter("android.sh");
+	// BufferedWriter out = new BufferedWriter(fstream);
+	//
+	// out.write(addSdkDir);
+	// out.newLine();
+	// out.write("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
+	// out.close();
+	// Runtime.getRuntime().exec("sh android.sh");
+	// Runtime.getRuntime().exec("ant debug");
+	// Runtime.getRuntime().exec("android create project  --target 1  --name Bazinga  --path ./MyAndroidAppProject  --activity MyAndroidAppActivity  --package com.bazinga.android");
+	// ProcessBuilder pb = new ProcessBuilder("android.sh");
+	// Map<String,String> env=pb.environment();
+	// Process p = pb.redirectErrorStream(true).start();
+	// p.getOutputStream().close();
+	// BufferedReader in = new BufferedReader(new InputStreamReader(p
+	// .getInputStream()));
+	// StringBuffer buffer = new StringBuffer();
+	// String line;
+	// while ((line = in.readLine()) != null) {
+	// buffer.append(line).append("\n");
+	// }
+	//
+	// int retval = p.waitFor();
+	//
+	// if (retval != 0) {
+	// throw new CommandException("The command execution returned "
+	// + retval + " as return value... !\n" + buffer);
+	// }
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// catch (InterruptedException e) {
+	// throw new CommandException("It seems the process was killed", e);
+	// }
+	// }
+
+	private String readLogCatFile() {
+		String returnString = null;
+		try {
+			String executeCommand = "adb logcat -d raw JUnitSysOut:I *:S";
+			Process waitForProcess = Runtime.getRuntime().exec(executeCommand);
+
+			waitForProcess.waitFor();
+			BufferedReader buf = new BufferedReader(new InputStreamReader(waitForProcess.getInputStream()));
+			String line = "";
+			String output = "";
+			while ((line = buf.readLine()) != null) {
+				output += line + "\n";
+			}
+
+			returnString = output;
+
+		} catch (IOException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		} catch (InterruptedException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+		return returnString.substring(returnString.indexOf(":") + 1);
+
+	}
 
 	@Override
 	public String getSourceCodeExtension() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	void installApkOnPhone()
-	{
-		try{
-			
+
+	void installApkOnPhone() {
+		try {
+
 			List<String> paramsToInstallApk = new ArrayList<String>();
-			ProcessBuilder pbForApk=new ProcessBuilder(paramsToInstallApk);
+			ProcessBuilder pbForApk = new ProcessBuilder(paramsToInstallApk);
 			paramsToInstallApk.add("adb");
 			paramsToInstallApk.add("install");
-			paramsToInstallApk.add(Settings.get("android_out_dir_final")+"//bin//AndroidProject-debug.apk");
+			paramsToInstallApk.add(Settings.get("android_out_dir_final")
+					+ "//bin//AndroidProject-debug.apk");
 			Process executeApk = pbForApk.redirectErrorStream(true).start();
 			int retValForApk = executeApk.waitFor();
 			if (retValForApk != 0) {
 				throw new CommandException("The command execution returned "
 						+ retValForApk + " as return value... !\n");
-						}
-			
-		}
-		catch (IOException e) {
+			}
+
+		} catch (IOException e) {
 			throw new CommandException(e);
 		} catch (InterruptedException e) {
 			throw new CommandException("It seems the process was killed", e);
 		}
 	}
-	
 
-	void runApkOnPhone()
-	{
-			List<String> paramsToRunApk = new ArrayList<String>();
-			paramsToRunApk.add("adb");
-			paramsToRunApk.add("shell");
-			paramsToRunApk.add("am");
-			paramsToRunApk.add("start");
-			paramsToRunApk.add("-n");
-			paramsToRunApk.add("com.google.test/.TestActivity");
-			String runApkResult = Command.exec(new File(Settings.get("android_sdk_path")), paramsToRunApk);
-			System.out.println(runApkResult);
-		}
+	void runApkOnPhone() {
+		List<String> paramsToRunApk = new ArrayList<String>();
+		paramsToRunApk.add("adb");
+		paramsToRunApk.add("shell");
+		paramsToRunApk.add("am");
+		paramsToRunApk.add("start");
+		paramsToRunApk.add("-n");
+		paramsToRunApk.add("com.google.test/.TestActivity");
+		String runApkResult = Command.exec(
+				new File(Settings.get("android_sdk_path")), paramsToRunApk);
+		System.out.println(runApkResult);
 	}
-	
+}
--- src/gool/test/GoolTest.java
+++ src/gool/test/GoolTest.java
@@ -17,36 +17,48 @@ import org.junit.Test;
 
 public class GoolTest {
 	private static Logger logger = Logger.getLogger(GoolTest.class);
-	
+
 	private static class GoolTestExecutor {
 		private static final String CLEAN_UP_REGEX = "Note:.*?[\r\n]|(\\w+>\\s)|[\\r\\n]+";
 		private String input;
 		private String expected;
+
 		public GoolTestExecutor(String input, String expected) {
 			this.input = input;
 			this.expected = expected;
 		}
+
 		public void compare(Platform platform) throws Exception {
+			// This inserts a package which is mandatory for android
+			// TODO Not the ideal place to put it also com.test should be in the
+			// properties file
+			if (platform instanceof AndroidPlatform) {
+				this.input = "package com.test; " + input;
+			}
 			String result = compileAndRun(platform);
 			logger.info(platform + " Result: " + result);
-			Assert.assertEquals(String.format("The platform %s", platform), expected, result);
+			Assert.assertEquals(String.format("The platform %s", platform),
+					expected, result);
 		}
+
 		protected String compileAndRun(Platform platform) throws Exception {
 			String cleanOutput = cleanOutput(TestHelper.generateCompileRun(
 					platform, input, MAIN_CLASS_NAME));
 			return cleanOutput;
 		}
-		
+
 		private static String cleanOutput(String result) {
-			return result.replaceAll(CLEAN_UP_REGEX, "")
-					.trim();
+			return result.replaceAll(CLEAN_UP_REGEX, "").trim();
 		}
 	}
-	
+
 	private static final String MAIN_CLASS_NAME = "Test";
-	private List<Platform> platforms =
-	 Arrays.asList(JavaPlatform.getInstance(),CppPlatform.getInstance(),CSharpPlatform.getInstance(), AndroidPlatform.getInstance());
-//	private List<Platform> platforms = Arrays.asList(CppPlatform.getInstance());
+	private List<Platform> platforms = Arrays.asList(
+			JavaPlatform.getInstance(), CppPlatform.getInstance(),
+			CSharpPlatform.getInstance(), AndroidPlatform.getInstance());
+
+	// private List<Platform> platforms =
+	// Arrays.asList(CppPlatform.getInstance());
 
 	@BeforeClass
 	public static void init() {
@@ -59,20 +71,20 @@ public class GoolTest {
 		String expected = "Hello World";
 		compareResultsDifferentPlatforms(input, expected);
 	}
-	
+
 	@Test
 	public void simpleChar() throws Exception {
 		String input = TestHelper.surroundWithClassMain(
-				"char testChar = 'A'; System.out.println(testChar);", MAIN_CLASS_NAME);
+				"char testChar = 'A'; System.out.println(testChar);",
+				MAIN_CLASS_NAME);
 		String expected = "A";
 		compareResultsDifferentPlatforms(input, expected);
 	}
-	
 
 	@Test
 	public void simpleAddition() throws Exception {
-		String input = TestHelper.surroundWithClassMain("System.out.println(2 + 2);",
-				MAIN_CLASS_NAME);
+		String input = TestHelper.surroundWithClassMain(
+				"System.out.println(2 + 2);", MAIN_CLASS_NAME);
 		String expected = "4";
 		compareResultsDifferentPlatforms(input, expected);
 	}
@@ -99,12 +111,11 @@ public class GoolTest {
 
 	@Test
 	public void listAddGet() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList<Integer> l = new ArrayList<Integer>(); l.add(4); System.out.println(l.get(0));",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList<Integer> l = new ArrayList<Integer>(); l.add(4); System.out.println(l.get(0));",
+								MAIN_CLASS_NAME);
 		logger.info(input);
 		String expected = "4";
 		compareResultsDifferentPlatforms(input, expected);
@@ -115,13 +126,13 @@ public class GoolTest {
 		// String input =
 		// TestHelper.surroundWithClassMain("HashMap<String, Integer > m = new HashMap<String, Integer>();",
 		// "Test");
-		String input = 
-				"import gool.imports.java.util.HashMap;\n" + 
+		String input = "import gool.imports.java.util.HashMap;\n"
+				+
 
 				TestHelper
-				.surroundWithClassMain(
-						"String four = \"four\"; HashMap<String, Integer > m = new HashMap<String, Integer>(); m.put(four,4); System.out.println(m.get(four));",
-						MAIN_CLASS_NAME);
+						.surroundWithClassMain(
+								"String four = \"four\"; HashMap<String, Integer > m = new HashMap<String, Integer>(); m.put(four,4); System.out.println(m.get(four));",
+								MAIN_CLASS_NAME);
 		String expected = "4";
 		compareResultsDifferentPlatforms(input, expected);
 	}
@@ -142,65 +153,68 @@ public class GoolTest {
 				"Printer p = new Printer(); p.print();", MAIN_CLASS_NAME);
 		input += "\n"
 				+ TestHelper.surroundWithClass(
-						"public void print(){System.out.println(2 + 2);}", "Printer", "");
+						"public void print(){System.out.println(2 + 2);}",
+						"Printer", "");
 		String expected = "4";
 		compareResultsDifferentPlatforms(input, expected);
 	}
 
 	@Test
 	public void simpleForEach() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"Integer total = 0;"
-								+ " ArrayList<Integer> l = new ArrayList<Integer>();"
-								+ " l.add(-2); l.add(-1);l.add(0); l.add(1); l.add(2);l.add(4);"
-								+ " for(Integer i : l){" + "total = total + i;"
-								+ "}" + "System.out.println(total);", MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"Integer total = 0;"
+										+ " ArrayList<Integer> l = new ArrayList<Integer>();"
+										+ " l.add(-2); l.add(-1);l.add(0); l.add(1); l.add(2);l.add(4);"
+										+ " for(Integer i : l){"
+										+ "total = total + i;" + "}"
+										+ "System.out.println(total);",
+								MAIN_CLASS_NAME);
 		String expected = "4";
 		compareResultsDifferentPlatforms(input, expected);
 	}
 
 	@Test
 	public void mapForEach() throws Exception {
-		String input = 
-				"import gool.imports.java.util.HashMap;\n" + 
-				TestHelper.surroundWithClassMain("Integer total = 0;"
-				+ " HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();"
-				+ " m.put(0, 1); m.put(2, 3);"
-				+ " for(HashMap.Entry<Integer, Integer> entry : m){"
-				+ "total = total + entry.getKey();"
-				+ "total = total + entry.getValue();" + "}"
-				+ "System.out.println(total);", MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.HashMap;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"Integer total = 0;"
+										+ " HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();"
+										+ " m.put(0, 1); m.put(2, 3);"
+										+ " for(HashMap.Entry<Integer, Integer> entry : m){"
+										+ "total = total + entry.getKey();"
+										+ "total = total + entry.getValue();"
+										+ "}" + "System.out.println(total);",
+								MAIN_CLASS_NAME);
 		String expected = "6";
 		compareResultsDifferentPlatforms(input, expected);
 	}
 
 	@Test
 	public void listWithDifferentTypeElement() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList l = new ArrayList();l.add(1);l.add(\"hola\");System.out.println(l.size());",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList l = new ArrayList();l.add(1);l.add(\"hola\");System.out.println(l.size());",
+								MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "2");
 	}
 
 	@Test
 	public void mapWithoutTypes() throws Exception {
 		try {
-			String input = 
-					"import gool.imports.java.util.HashMap;\n" + 
-					TestHelper
-					.surroundWithClassMain(
-							"HashMap m = new HashMap();m.put(0, 1);m.put(\"hola\", 2);System.out.println(m.size());",
-							MAIN_CLASS_NAME);
+			String input = "import gool.imports.java.util.HashMap;\n"
+					+ TestHelper
+							.surroundWithClassMain(
+									"HashMap m = new HashMap();m.put(0, 1);m.put(\"hola\", 2);System.out.println(m.size());",
+									MAIN_CLASS_NAME);
 			compareResultsDifferentPlatforms(input, "2");
-		} 
-		catch (Exception e) {
-			if (e.getCause() != null && e.getCause().getClass().equals(IllegalStateException.class)) {
+		} catch (Exception e) {
+			if (e.getCause() != null
+					&& e.getCause().getClass()
+							.equals(IllegalStateException.class)) {
 				return;
 			}
 			logger.error(e);
@@ -210,118 +224,114 @@ public class GoolTest {
 
 	@Test
 	public void removeElementsFromUntypedList() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList l = new ArrayList();l.add(\"\");l.add(\"hola\");l.remove(\"hola\");System.out.println(l.size());",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList l = new ArrayList();l.add(\"\");l.add(\"hola\");l.remove(\"hola\");System.out.println(l.size());",
+								MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "1");
 	}
 
 	@Test
 	public void removeElementsFromIntegerList() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList<Integer> l = new ArrayList<Integer>();l.add(1);l.add(4);l.removeAt(1);System.out.println(l.size());",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList<Integer> l = new ArrayList<Integer>();l.add(1);l.add(4);l.removeAt(1);System.out.println(l.size());",
+								MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "1");
 	}
 
 	@Test
 	public void removeElementsFromMap() throws Exception {
-		String input = 
-				"import gool.imports.java.util.HashMap;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();m.put(1, 2);m.put(2, 3);m.remove(2);System.out.println(m.size());",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.HashMap;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();m.put(1, 2);m.put(2, 3);m.remove(2);System.out.println(m.size());",
+								MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "1");
 	}
-	
+
 	@Test
 	public void isEmptyList() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList l = new ArrayList();l.add(\"hola\");l.remove(\"hola\");System.out.println(l.isEmpty());",
-						MAIN_CLASS_NAME);
-		compareResultsDifferentPlatforms(new GoolTestExecutor(input, "true"){
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList l = new ArrayList();l.add(\"hola\");l.remove(\"hola\");System.out.println(l.isEmpty());",
+								MAIN_CLASS_NAME);
+		compareResultsDifferentPlatforms(new GoolTestExecutor(input, "true") {
 			@Override
 			protected String compileAndRun(Platform platform) throws Exception {
 				String output = super.compileAndRun(platform).toLowerCase();
-				
+
 				// C++ does not have booleans
 				if ("1".equals(output)) {
 					output = "true";
 				} else if ("0".equals(output)) {
 					output = "false";
 				}
-				
+
 				return output;
 			}
 		});
 	}
-	
+
 	@Test
 	public void listContainsElement() throws Exception {
-		String input = 
-				"import gool.imports.java.util.ArrayList;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"ArrayList l = new ArrayList();l.add(\"hola\");l.remove(\"hola\");l.add(\"hola\");System.out.println(l.contains(\"hola\"));",
-						MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.util.ArrayList;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"ArrayList l = new ArrayList();l.add(\"hola\");l.remove(\"hola\");l.add(\"hola\");System.out.println(l.contains(\"hola\"));",
+								MAIN_CLASS_NAME);
 		Assert.fail("Not implemented");
 	}
-	
+
 	@Test
 	public void classWithAttributes() throws Exception {
-		String input = "class Test {" +
-				"public int z; public Test(int i){this.z=i+2;}" +
-				"public static void main(String[] args){" +
-				"System.out.println(new Test(5).z);"+
-				"}}";
+		String input = "class Test {"
+				+ "public int z; public Test(int i){this.z=i+2;}"
+				+ "public static void main(String[] args){"
+				+ "System.out.println(new Test(5).z);" + "}}";
 		compareResultsDifferentPlatforms(input, "7");
 	}
 
 	@Test
 	public void unknownOperator() throws Exception {
-		String input = TestHelper
-				.surroundWithClassMain(
-						"int total = 1 ^ 0; System.out.println(total);",
-						MAIN_CLASS_NAME);
+		String input = TestHelper.surroundWithClassMain(
+				"int total = 1 ^ 0; System.out.println(total);",
+				MAIN_CLASS_NAME);
 		String expected = "";
 		compareResultsDifferentPlatforms(input, "expected");
 	}
+
 	@Test
 	public void fileTest() throws Exception {
-		String input = 
-				"import gool.imports.java.io.BufferedReader;\n" +"import gool.imports.java.io.FileReader;\n"+ 
-				TestHelper
-				.surroundWithClassMain(
-						"BufferedReader br = new BufferedReader(new FileReader(\"/GOOLOUTPUTJAVA/src/test.txt\"));while((br.readLine()) != null) {}",MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.io.BufferedReader;\n"
+				+ "import gool.imports.java.io.FileReader;\n"
+				+ TestHelper
+						.surroundWithClassMain(
+								"BufferedReader br = new BufferedReader(new FileReader(\"/GOOLOUTPUTJAVA/src/test.txt\"));while((br.readLine()) != null) {}",
+								MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "");
 	}
+
 	@Test
 	public void simpleFileTest() throws Exception {
-		String input = 
-				"import gool.imports.java.io.File;\n" + 
-				TestHelper
-				.surroundWithClassMain(
-						"File f = new File(\"a.txt\");",MAIN_CLASS_NAME);
+		String input = "import gool.imports.java.io.File;\n"
+				+ TestHelper.surroundWithClassMain(
+						"File f = new File(\"a.txt\");", MAIN_CLASS_NAME);
 		compareResultsDifferentPlatforms(input, "");
 	}
-	private void compareResultsDifferentPlatforms(String input,
-			String expected) throws Exception {
+
+	private void compareResultsDifferentPlatforms(String input, String expected)
+			throws Exception {
 		compareResultsDifferentPlatforms(new GoolTestExecutor(input, expected));
 	}
-	
-	private void compareResultsDifferentPlatforms(GoolTestExecutor executor) throws Exception {
+
+	private void compareResultsDifferentPlatforms(GoolTestExecutor executor)
+			throws Exception {
 		for (Platform platform : platforms) {
 			executor.compare(platform);
 		}
-	}	
+	}
 }
