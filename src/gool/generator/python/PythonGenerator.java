fd5c4e94fa5ab71ca960cb86e1c380d24bd3b316
NicolasBouscarle <nicolas.bouscarle@centraliens-lille.org>
2013-01-22 11:02:12 +0100

    python deals with 'super'

--- src/gool/generator/python/PythonGenerator.java
+++ src/gool/generator/python/PythonGenerator.java
@@ -14,6 +14,7 @@ import gool.ast.constructs.CustomDependency;
 import gool.ast.constructs.Dependency;
 import gool.ast.constructs.EnhancedForLoop;
 import gool.ast.constructs.EqualsCall;
+import gool.ast.constructs.Expression;
 import gool.ast.constructs.ExpressionUnknown;
 import gool.ast.constructs.Field;
 import gool.ast.constructs.For;
@@ -94,6 +95,8 @@ public class PythonGenerator extends CommonCodeGenerator {
 		indentation = "    ";
 	}
 	
+	private ClassDef currentClass;
+	
 	private ArrayList<String> comments = new ArrayList<String>();
 	
 	private ArrayList<String> paramsMethCurrent = new ArrayList<String>();
@@ -130,8 +133,10 @@ public class PythonGenerator extends CommonCodeGenerator {
 	
 	@Override
 	public String getCode(ArrayNew arrayNew) {
-		return String.format("%s[%s]", arrayNew.getType(), StringUtils
-				.join(arrayNew.getDimesExpressions(), ", "));
+		String ret = arrayNew.getType() + "()";
+		for (Expression e : arrayNew.getDimesExpressions())
+			ret = String.format("[%s]*%s", ret, e);
+		return "(" + ret + ")";
 	}
 	
 	@Override
@@ -397,6 +402,8 @@ public class PythonGenerator extends CommonCodeGenerator {
 		if(varAccess.getType() == null || varAccess.getType().getName().isEmpty()) {
 			return name;
 		}
+		if (name.equals("super") && currentClass != null)
+			return String.format("super(%s, self)", currentClass.getName());
 		if (name.equals("this"))
 			return "self";
 
@@ -406,7 +413,7 @@ public class PythonGenerator extends CommonCodeGenerator {
 		if (paramsMethCurrent.contains(name))
 			return name;
 		else
-			return name;
+			return "self." + name;
 			
 	}
 
@@ -643,7 +650,7 @@ public class PythonGenerator extends CommonCodeGenerator {
 
 	@Override
 	public String getCode(ExpressionUnknown unknownExpression) {
-		comment ("Unrecognized by GOOL, passed on: " + unknownExpression.getTextual());
+		comment ("Unrecognized by GOOL, expression passed on");
 		return unknownExpression.getTextual();
 	}
 
@@ -661,6 +668,8 @@ public class PythonGenerator extends CommonCodeGenerator {
 	
 	@Override
 	public String printClass(ClassDef classDef) {
+		currentClass = classDef;
+		
 		// every python script has to start with a hash-bang:
 		// do not change the "#!/usr/bin/env python" line!
 		StringBuilder code = new StringBuilder ("#!/usr/bin/env python\n\nimport goolHelper\n\n");
