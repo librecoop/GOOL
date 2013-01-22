339d07c418eb2c9f860aa9b3a611073a83d0050c
montantj <montantj@pcnt-217-12>
2013-01-22 13:06:52 +0100

    Ajout du fichier GeneratorHelperObjc, il permet de choisir le format, on une syntaxe particulière de type. => Modification du Generatorhelper pour qu'il ne soit plus final : attention peut petre problèmatique d'un point de vue Objet

---
+++ src/gool/generator/objc/GeneratorHelperObjc.java
@@ -0,0 +1,63 @@
+package gool.generator.objc;
+
+import gool.ast.constructs.BinaryOperation;
+import gool.ast.constructs.Expression;
+import gool.ast.type.IType;
+import gool.ast.type.TypeBool;
+import gool.ast.type.TypeChar;
+import gool.ast.type.TypeClass;
+import gool.ast.type.TypeDecimal;
+import gool.ast.type.TypeInt;
+import gool.ast.type.TypeString;
+import gool.generator.GeneratorHelper;
+
+public final class GeneratorHelperObjc extends GeneratorHelper {
+	
+	public static String type(IType type) {
+		if(type instanceof TypeInt){
+			return "Int";
+		}
+		else if(type instanceof TypeChar){
+			return "Char";
+		}
+		else if(type instanceof TypeDecimal){
+			return "Double";
+		}
+		else if(type instanceof TypeBool){
+			return "Bool";
+		}
+		else{
+			return "PROBLEM";
+		}
+	}
+	
+	public static String format(Expression e){
+		if(e.getType().equals(TypeString.INSTANCE)){
+			return "%@";
+		}
+		else if(e.getType().equals(TypeInt.INSTANCE)){
+			return "%d";
+		}
+		else if(e.getType().equals(TypeChar.INSTANCE)){
+			return "%c";
+		}
+		else if(e.getType().equals(TypeDecimal.INSTANCE)){
+			return "%f";
+		}
+		else if(e.getType().equals(TypeBool.INSTANCE)){
+			return "%d";
+		}
+		else if(e.getType() instanceof TypeClass){
+			return "%@";
+		}
+		return null;
+	}
+	
+	public static String evalIntExpr(Expression e){
+		if(e instanceof BinaryOperation)
+			return "(" + evalIntExpr(((BinaryOperation) e).getLeft()) + ((BinaryOperation) e).getTextualoperator() + evalIntExpr(((BinaryOperation) e).getRight()) + ")";
+		else 
+			return e.toString();
+	}
+	
+}
