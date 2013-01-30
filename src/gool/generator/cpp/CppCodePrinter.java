6a0ef60c11b374b74a81e89ac430936a9afc949d
Gaël PICOT <gael.picot@free.fr>
2013-01-30 11:06:11 +0100

    Exception OK in CPP

--- src/gool/generator/cpp/CppCodePrinter.java
+++ src/gool/generator/cpp/CppCodePrinter.java
@@ -8,12 +8,18 @@ package gool.generator.cpp;
 
 import gool.ast.constructs.ClassDef;
 import gool.generator.common.CodePrinter;
+import gool.generator.python.PythonPlatform;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import logger.Log;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -23,9 +29,41 @@ import org.apache.commons.lang.StringUtils;
  */
 public class CppCodePrinter extends CodePrinter {
 	private static final String TEMPLATE_DIR = "gool/generator/cpp/templates/";
+	
+	private void createFinallyInclude(File outputDir) {
+		FileOutputStream goolHelperOut;
+		byte[] buffer = new byte[1024];
+		int noOfBytes;
+
+		// Helpers to create by copying the resource
+		List<String> goolHelperIn = new ArrayList<String>();
+		goolHelperIn.add("finally.h");
+		if (! outputDir.isDirectory() && ! outputDir.mkdirs()) {
+			Log.e(String.format("Impossible to create the directory '%s'", outputDir));
+		} else {
+			// Print finally
+			for(String in : goolHelperIn) {
+				InputStream helper;
+				try {
+					helper = CppPlatform.class.getResource(in).openStream();
+	
+					goolHelperOut = new FileOutputStream (outputDir+"/"+in);
+					while ((noOfBytes = helper.read(buffer)) != -1) {
+						goolHelperOut.write(buffer, 0, noOfBytes);
+					}
+					goolHelperOut.close();
+					helper.close();
+				} catch (IOException e){
+					Log.e(String.format("Impossible to create the file '%s'", in));
+				}
+			}
+		}
+	}
+		
 
 	public CppCodePrinter(File outputDir) {
 		super(new CppGenerator(), outputDir);
+		createFinallyInclude(outputDir);
 	}
 
 	@Override
