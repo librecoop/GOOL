3d519907969171b451614f6b64f19580f89cef81
NicolasBouscarle <nicolas.bouscarle@centraliens-lille.org>
2013-01-31 11:21:33 +0100

    support for unicode strings in goolHelper.test_args

--- src/gool/generator/python/goolHelper/__init__.py
+++ src/gool/generator/python/goolHelper/__init__.py
@@ -1,5 +1,10 @@
 def test_args(args,*types):
     for a, t in zip(args, types):
+        if t == str:
+            try:
+                t = basestring
+            except:
+                pass
         if not isinstance(a, t):
             return False
     return len(args) == len(types)
