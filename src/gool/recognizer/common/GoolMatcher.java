e490004e561903c1619ce04768830e46d7e0eade
KevinMazet <kevin.mazet@gmail.com>
2013-05-30 16:57:32 +0200

    before adding GPL terms in headers of each file

---
+++ src/gool/recognizer/common/GoolMatcher.java
@@ -0,0 +1,171 @@
+package gool.recognizer.common;
+
+import gool.ast.constructs.Language;
+import gool.imports.java.util.ArrayList;
+
+public class GoolMatcher{
+	
+	//contains PATHs to the GOOL libraries enabled for the current translation process
+	private static ArrayList<String> GoolLibsEnabled;
+	
+	//contains PATHs to the GOOL libraries used during the current translation process
+	private static ArrayList<String> GoolLibsUsed;
+	
+	GoolMatcher(){
+		GoolLibsEnabled = new ArrayList<String>();
+		GoolLibsUsed = new ArrayList<String>();
+	}
+	
+	
+	public void clearGoolLibs(){
+		this.GoolLibsUsed = new ArrayList<String>();
+	}
+	
+	public void addDefaultLibs(Language inputLanguage){
+		
+		
+	}
+	
+	
+	// the following methods compute the path to match files
+	private String getPathOfInputMatchDir(String GoolLibName, Language inputLang){
+		return "src/gool/recognizer/" + langToString(inputLang).toLowerCase() + "/matching/"+GoolLibName+"/";
+	}
+	private String getPathOfInputPackageMatchFile(Language inputLang){
+		return "src/gool/recognizer/" + langToString(inputLang).toLowerCase() + "/matching/"
+	+ langToString(inputLang) + "PackageMatching.properties";
+	}
+	private String getPathOfInputClassMatchFile(String GoolLibName, Language inputLang){
+		return getPathOfInputMatchDir(GoolLibName, inputLang) + langToString(inputLang) + "ClassMatching.properties";
+	}
+	private String getPathOfInputMethodMatchFile(String GoolLibName, Language inputLang){
+		return getPathOfInputMatchDir(GoolLibName, inputLang) + langToString(inputLang) + "MethodMatching.properties";
+	}
+	
+	private String getPathOfOutputMatchPath(String GoolLibName, Language outputLang){
+		return "src/gool/generator/" + langToString(outputLang).toLowerCase() + "/matching/"+GoolLibName+"/";
+	}
+	private String getPathOfOutputClassMatchFile(String GoolLibName, Language outputLang){
+		return getPathOfOutputMatchPath(GoolLibName, outputLang) + langToString(outputLang) + "ClassMatching.properties";
+	}
+	private String getPathOfOutputMethodMatchFile(String GoolLibName, Language outputLang){
+		return getPathOfOutputMatchPath(GoolLibName, outputLang) + langToString(outputLang) + "MethodMatching.properties";
+	}
+	private String getPathOfOutputDependencyFile(String GoolLibName, Language outputLang){
+		return getPathOfOutputMatchPath(GoolLibName, outputLang) + langToString(outputLang) + "Dependencies.properties";
+	}
+	private String getPathOfOutputCustomizedMethodFile(String GoolLibName, Language outputLang, MethodSignature s){
+		return getPathOfOutputMatchPath(GoolLibName, outputLang) + "CustomizedMethods/" + s;
+	}
+	
+	
+	// translation of a Language to a String
+	private String langToString(Language lang){
+		String res = "";
+		switch(lang){
+		case JAVA:
+			res = "Java";
+			break;
+		case CPP:
+			res = "Cpp";
+			break;
+		case CSHARP:
+			res = "CSharp";
+			break;
+		case OBJC:
+			res = "Objc";
+			break;
+		case PYTHON:
+			res = "Python";
+			break;
+		case ANDROID:
+			res = "Android";
+			break;
+		}
+		return res;
+	}
+	
+	// removing spaces and tabulations from a String
+	private String removeSpaces(String s){
+		for(int i=0; i<s.length(); i++){
+			if(s.charAt(i)==' ' || s.charAt(i)=='\t'){
+				s=s.substring(0,i)+s.substring(i+1);
+				i-=1;
+			}
+		}
+		return s;
+	}
+	// this class is used by the GoolMatcher to compare methods
+	class MethodSignature{
+		String classname;
+		String methodname;
+		ArrayList<String> paramtypes;
+		String returntype;
+		MethodSignature(String className, String methodName, ArrayList<String> paramtypes, String returntype){
+			this.classname=className;
+			this.methodname=methodName;
+			this.paramtypes=paramtypes;
+			this.returntype=returntype;
+		}
+		// a method signature can be created from a raw string such as: "methodName(int,char):int"
+		MethodSignature(String RawSignature){
+			String s=removeSpaces(RawSignature);
+			String sname=s.substring(0, s.indexOf("("));
+			
+			System.out.println("Partie nom de classe et nom de méthode: "+sname);
+			
+			this.classname=sname.substring(0, sname.lastIndexOf("."));
+			sname=sname.substring(sname.lastIndexOf("."));
+			this.methodname=sname.substring(1);
+			s=s.substring(s.indexOf("(")+1);
+			System.out.println("Nom de classe: "+classname);
+			System.out.println("Nom de methode: "+methodname);
+			System.out.println("Partie paramètres et valeur de retour après la première parenthèse: "+s);
+			paramtypes=new ArrayList<String>();
+			for(int i=0; s.charAt(0)!=':'; i++){
+				int ind1=s.indexOf(",");
+				int ind2=s.indexOf(")");
+				if(ind1!=-1){
+					this.paramtypes.add(s.substring(0, ind1));
+					s=s.substring(ind1+1);
+				}
+				else{
+					this.paramtypes.add(s.substring(0, ind2));
+					s=s.substring(ind2+1);
+				}
+			}
+			s=s.substring(1);
+			this.returntype=s;
+		}
+		// 2 signatures are compatible if they have exactly the same return type and the same parameter types
+		public boolean isCompatibleWith(MethodSignature s){
+			if(!this.returntype.equals(s.returntype))
+				return false;
+			if(this.paramtypes.size()!=s.paramtypes.size())
+				return false;
+			for(int i=0; i<this.paramtypes.size(); i++)
+				if(!this.paramtypes.get(i).equals(s.paramtypes.get(i)))
+					return false;
+			return true;
+		}
+		// 2 signatures are equal if they have exactly the same class name, method name, parameter types and return type
+		public boolean equals(MethodSignature s){
+			if(!this.classname.equals(s.classname))
+				return false;
+			if(!this.methodname.equals(s.classname))
+				return false;
+			if(!this.isCompatibleWith(s))
+				return false;
+			return true;
+		}
+		public String toString(){
+			String res = this.classname + "." + this.methodname + "(";
+			for(String paramtype: this.paramtypes)
+				res += paramtype + ",";
+			if(!this.paramtypes.isEmpty())
+				res = res.substring(0,res.length()-1);
+			res += "):" + this.returntype;
+			return res;
+		}
+	}
+}
