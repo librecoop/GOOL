6556db5d149401a80bfc5a0dcd1dd07f0ef1dec9
KevinMazet <kevin.mazet@gmail.com>
2013-06-04 11:46:23 +0200

    still working on GoolMatcher

---
+++ tests/libTest/test/Test.java
@@ -0,0 +1,12 @@
+//import java.io.*;
+public class Test{
+	public static void main(String []args) throws Exception{
+		/*InputStream ips= new FileInputStream("/home/zalgo/test.txt"); 
+		InputStreamReader ipsr=new InputStreamReader(ips);
+		BufferedReader br=new BufferedReader(ipsr);
+		br.close();*/
+		String s=new String();
+		s.contains("");
+		s.getBytes();
+	}
+}
